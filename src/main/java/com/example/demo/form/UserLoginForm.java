72c4f499780524fd5ead3b49695fda4d7a5fd560
NahataManami <manami.nahata@rakus-partners.co.jp>
2022-02-17 12:16:17 +0900

    ◎バリデーションの削除 　→メールアドレスとパスワードそれぞれにバリデーションをつけるのではなく、「メールアドレスまたはパスワードが不正です。」と表示する。
    ・UserLoginForm
    ・login.html

--- src/main/java/com/example/demo/form/UserLoginForm.java
+++ src/main/java/com/example/demo/form/UserLoginForm.java
@@ -1,9 +1,5 @@
 package com.example.demo.form;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 /**
  * ログインフォームからメールアドレスとパスワードを受け取る。
  * @author manami
@@ -11,11 +7,7 @@ import javax.validation.constraints.Size;
  */
 public class UserLoginForm {
 	
-	@NotBlank(message = "メールアドレスを入力してください。")
-	@Email(message = "メールアドレスの形式で入力してください。")
 	private String userEmail;
-	@NotBlank(message = "パスワードを入力してください。")
-	@Size(min = 8, max = 16, message = "パスワードは8文字以上16文字以下で入力してください。")
 	private String userPassword;
 	
 	public String getUserEmail() {
