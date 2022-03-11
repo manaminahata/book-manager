7150d5665cc1584a8ef93d6d7af0edf2073a5e47
NahataManami <manami.nahata@rakus-partners.co.jp>
2022-03-11 17:50:31 +0900

    【仮】DBに書籍を追加するためにControllerとServiceクラスを作成

---
+++ src/main/java/com/example/demo/controller/AddBooksController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * DBに書籍を追加する
+ * @author manami
+ *
+ */
+@Controller
+@RequestMapping("/yonda")
+public class AddBooksController {
+
+}
---
+++ src/main/java/com/example/demo/service/AddBooksService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Book;
+
+/**
+ * DBに書籍を追加する
+ * @author manami
+ *
+ */
+@Service
+public class AddBooksService {
+	
+}
